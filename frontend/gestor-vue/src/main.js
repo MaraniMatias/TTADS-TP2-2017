9e52f32499645dd47e2394a91506e76153e58992
andres1790 <andresdlg21@gmail.com>
2017-11-13 23:35:39 -0300

    agregada la base para gestor con vue

--- frontend/gestor-vue/src/main.js
+++ frontend/gestor-vue/src/main.js
@@ -1,7 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import equipos from './components/equipos.vue'
+import partidos from './components/partidos.vue'
 
-new Vue({
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  //mode: 'history', //navega sin recargar pag y sin #, requiere config. el backend
+  routes: [ 
+    {
+      name: "equipos",
+      path: '/equipos',
+      component: equipos
+    },
+    {
+      name: "partidos",
+      path: '/partidos',
+      component: partidos
+    }
+  ]
+});
+
+export default new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
