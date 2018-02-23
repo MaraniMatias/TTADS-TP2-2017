2e17774dfce00a758dc593d605d4e1cf00e74ace
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-02-23 11:57:20 -0300

    Add pager for endpont jugadores

--- backend/routes/api/apiJugadores.js
+++ backend/routes/api/apiJugadores.js
@@ -1,25 +1,59 @@
+"use strict";
 const express = require('express');
 const router = express.Router();
-const _ = require('loadsh');
+const _ = require('lodash');
 const Jugador = require('../../models/jugador');
+
+// Solo con el objetivo de enviar siempre una misma respuesta
+function sendRes(res, cod, data, message, error) {
+  res.status(cod);
+  return res.json({ data, message, error });
+}
+
+function queryPage(req, res, next) {
+  // en caso de no estar definido se fuersa a 10
+  const skip = _.get(req, 'query.skip', 2) || 2;
+  // en caso de no estar definido se fuersa a 10
+  const limit = _.get(req, 'query.limit', 2) || 2;
+  req.query.skip = skip;
+  req.query.limit = limit;
+  // Continuar con la consulta ala API
+  next();
+}
 
 // Recupera todos los jugadores
 // Buscar jugadores por nomnbre o apellido
 // query parameter skip limit player
-router.get('/jugadores',function(req,res){
-  if(req.query.player != undefined){
-    Jugador.find({$or:[
-      {nombre:{$regex: req.query.player }},
-      {apellido:{$regex: req.query.player }}
-    ]}).then(function(jugadores){
-      res.status(200).send(jugadores)
-    });
-  }else{
-    Jugador.find({}).then(function(jugadores){
-      res.status(200).send(jugadores)
-    });
-  }
-});
+router.get('/jugadores',
+  queryPage, // interceptor para completar el paginado
+  function (req, res) {
+    // Validar parámetro de la consulta
+    const player = _.get(req, 'query.jugador', false) || false;
+
+    if (player) {
+      Jugador.find({
+          $or: [
+            { nombre: { $regex: player } },
+            { apellido: { $regex: player } }
+          ]
+        })
+        .sort('apellido')
+        .skip(req.query.skip)
+        .limit(req.query.limit)
+        .exec(function (err, jugadores) {
+          if (err) {
+            // res, status, data, messager, error
+            return sendRes(res, 500, null, "Ha ocurrido un error", err);
+          } else {
+            // res, status, data, messager, error
+            return sendRes(res, 200, jugadores, "Success", null);
+          }
+        });
+    } else {
+      // res, status, data, messager, error
+      return sendRes(res, 402, null, "Parametro 'jugador' es requerido", null);
+    }
+  });
 
 router.get('/jugadores/:id',function(req,res){
   Jugador.findById({_id: req.params.id}).then(function(jugadores){
