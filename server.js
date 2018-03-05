89356b9aaa8391ee3b4c23d935cbeb09770be82a
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-03-04 22:06:42 -0300

    Run test ok

--- server.js
+++ server.js
@@ -66,9 +66,8 @@ app.use('/api', require('./routes/api/apiFixture'));
 // Static, FronEnd
 // NOTE: Por ahora no tocar, la uso en el serve
 app.use('/', function (req, res) {
-  res.status(200);
+  res.end('Server running');
 });
-
 
 app.use('/status', function (req, res) {
   res.json({
